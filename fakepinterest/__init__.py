abdca3c3507b0b764176b4997c97d2df055d9c60
Jonas da Silva Rabelo <130485427+JonasRabelo@users.noreply.github.com>
2023-08-11 16:31:47 -0300

    Adicionando mais bibliotecas

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -2,10 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy  # pip install flask_sqlalchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
-
+import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SECRET_KEY"] = "ecea26f18129dc774c669c3451c66c17"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
